f4333eb6dff830010350f7e9c10513b238ea872c
Topu Roy <topuroy143333@gmail.com.com>
2023-02-01 22:33:55 +0600

    Added CPAGrip Landing Page

---
+++ United States/index.js
@@ -0,0 +1,96 @@
+
+
+// * Country name
+// const country = document.querySelectorAll(".country_name");
+
+// * Target Devices 
+// const globalDevices = document.querySelector(".device");
+
+// * Card 1
+const heading1 = document.getElementById('heading_1');
+const subHeading1 = document.getElementById('sub_heading_1');
+const device1 = document.getElementById('device_1');
+const img1 = document.getElementById('card_img_1');
+const link1 = document.getElementById('link_1');
+
+// * Card 2
+const heading2 = document.getElementById('heading_2');
+const subHeading2 = document.getElementById('sub_heading_2');
+const device2 = document.getElementById('device_2');
+const img2 = document.getElementById('card_img_2');
+const link2 = document.getElementById('link_2');
+
+// * Card 3
+const heading3 = document.getElementById('heading_3');
+const subHeading3 = document.getElementById('sub_heading_3');
+const device3 = document.getElementById('device_3');
+const img3 = document.getElementById('card_img_3');
+const link3 = document.getElementById('link_3');
+
+// * Card 4
+const heading4 = document.getElementById('heading_4');
+const subHeading4 = document.getElementById('sub_heading_4');
+const device4 = document.getElementById('device_4');
+const img4 = document.getElementById('card_img_4');
+const link4 = document.getElementById('link_4');
+
+
+// * Card 5
+const heading5 = document.getElementById('heading_5');
+const subHeading5 = document.getElementById('sub_heading_5');
+const device5 = document.getElementById('device_5');
+const img5 = document.getElementById('card_img_5');
+const link5 = document.getElementById('link_5');
+
+
+// * card 6
+const heading6 = document.getElementById('heading_6');
+const subHeading6 = document.getElementById('sub_heading_6');
+const device6 = document.getElementById('device_6');
+const img6 = document.getElementById('card_img_6');
+const link6 = document.getElementById('link_6');
+
+
+// ! ----------------------------- Value settings ----------------------------- //
+
+// globalDevices.innerHTML = "Android Only";
+// country.innerHTML = "United States";
+
+
+heading1.innerHTML = "Act Now for a $750 PayPal Gift Card!";
+subHeading1.innerHTML = "Enter your information now for a chance to win.";
+img1.src = "./img/Act Now for a $750 PayPal Gift Card!.webp";
+device1.innerHTML = "Desktop Only";
+link1.href = "https://tundrafile.com/show.php?l=0&u=1105693&id=39550&tracking_id=";
+
+heading2.innerHTML = " Grab a $750 PayPal Gift Card Now! ";
+subHeading2.innerHTML = "Enter your information now for a chance to win.";
+img2.src = "./img/Act Now for a $750 PayPal Gift Card!.webp";
+device2.innerHTML = "Android Only";
+link2.href = "https://tundrafile.com/show.php?l=0&u=1105693&id=39551&tracking_id=";
+
+heading6.innerHTML = " Grab a $750 PayPal Gift Card Now! ";
+subHeading6.innerHTML = "Enter your information now for a chance to win. ";
+img6.src = "./img/Act Now for a $750 PayPal Gift Card!.webp";
+device6.innerHTML = "Iphone Only";
+link6.href = "https://tundrafile.com/show.php?l=0&u=1105693&id=49867&tracking_id=";
+
+heading3.innerHTML = "Enter for $10,000 Now! ";
+subHeading3.innerHTML = "Enter your information now for a chance to win. ";
+img3.src = "./img/Enter for $10,000 Now!.webp";
+device3.innerHTML = "Desktop Only";
+link3.href = "https://tundrafile.com/show.php?l=0&u=1105693&id=48179&tracking_id=";
+
+heading4.innerHTML = "Grab Your $10,000 Now! ";
+subHeading4.innerHTML = "Enter your information now for a chance to win. ";
+img4.src = "./img/Enter for $10,000 Now!.webp";
+device4.innerHTML = "Iphone Only";
+link4.href = "https://tundrafile.com/show.php?l=0&u=1105693&id=48181&tracking_id=";
+
+heading5.innerHTML = "Grab Your $10,000 Now! ";
+subHeading5.innerHTML = "Enter your information now for a chance to win. ";
+img5.src = "./img/Enter for $10,000 Now!.webp";
+device5.innerHTML = "Android Only";
+link5.href = "https://tundrafile.com/show.php?l=0&u=1105693&id=48180&tracking_id=";
+
+
